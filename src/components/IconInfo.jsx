d643d7a11a77cc912f1a119c6ea4ac5b036898bc
SamNinga <ninga254@gmail.com>
2023-08-13 21:53:15 +0300

    Adding component display name

--- src/components/IconInfo.jsx
+++ src/components/IconInfo.jsx
@@ -1,6 +1,10 @@
-export default ({ text, icon }) => (
+const IconText = ({ text, icon }) => (
     <div className="text-center mx-auto">
         {icon}
         <h6 className="font-medium">{text}</h6>
     </div>
-)
+);
+
+IconText.displayName = 'IconText';
+
+export default IconText;
